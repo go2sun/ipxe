a8255390c284079394e99d0c02b509330d4343e1
zwj4031 <dnyl@qq.com>
2019-08-05 21:42:22 +0800

    myipxe

---
+++ README.md
@@ -0,0 +1,2 @@
+# myipxe
+ipxe
---
+++ 复制.bat
@@ -0,0 +1,10 @@
+copy src\bin\undionly.pxe F:\pxeboot\undionly.pxe.0 /y
+copy src\bin\undionly.kpxe F:\pxeboot\undionly.kpxe.0 /y
+copy src\bin\undionly.kkpxe F:\pxeboot\undionly.kkpxe.0 /y
+copy src\bin\undionly.kkkpxe F:\pxeboot\undionly.kkkpxe.0 /y
+
+copy src\bin\undionly.pxe out\tinycore.pxe.0 /y
+copy src\bin\undionly.kpxe out\tinycore.kpxe.0 /y
+copy src\bin\undionly.kkpxe out\tinycore.kkpxe.0 /y
+copy src\bin\undionly.kkkpxe out\tinycore.kkkpxe.0 /y
+pause
