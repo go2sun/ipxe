78df36136f683b1585564434ceded6008e472bb3
zwj4031 <dnyl@qq.com>
2019-08-08 20:55:20 +0800

    ipxeloadefi2

--- kpxe - 副本.sh
+++
@@ -1,8 +0,0 @@
- cd src
- make bin/undionly.pxe EMBED=tinycore.ipxe
- make bin/undionly.kpxe EMBED=tinycore.ipxe
- make bin/undionly.kkpxe EMBED=tinycore.ipxe
-make bin/undionly.kkkpxe EMBED=tinycore.ipxe
-   
-
-
---
+++ kpxev2.sh
@@ -0,0 +1,8 @@
+ cd src
+ make bin/undionly.pxe EMBED=tinycorev2.ipxe
+ make bin/undionly.kpxe EMBED=tinycorev2.ipxe
+ make bin/undionly.kkpxe EMBED=tinycorev2.ipxe
+make bin/undionly.kkkpxe EMBED=tinycorev2.ipxe
+   
+
+
