e26083a65bc4c6cf881d04168c6af1ebb0c637c0
江南一根葱 <dnyl@qq.com>
2020-03-20 12:27:05 +0800

    Update loadefi.sh

--- loadefi.sh
+++ loadefi.sh
@@ -2,7 +2,7 @@
  cp config/console-efi.h config/console.h 
  cp config/general-efi.h config/general.h 
  make bin-x86_64-efi/ipxe.efi EMBED=ipxeloadefi
- make bin-x86_64-efi/snponly.efi
+ make bin-x86_64-efi/snponly.efi EMBED=ipxeloadefi
  
    
 
