59c93b43bcaa87c0941c46215d632fd9166f9901
zwj4031 <dnyl@qq.com>
2020-03-23 10:45:39 +0800

    make efidrv

--- uefi.sh
+++ uefi.sh
@@ -4,5 +4,7 @@
  cp config/general-efi.h config/general.h 
  make bin-x86_64-efi/ipxe.efi
  make bin-x86_64-efi/snponly.efi
- 
-   
+ make bin-x86_64-efi/ipxe.efidrv
+ make bin-x86_64-linux/tap.linux
+ make bin-x86_64-linux/tests.linux
+ make bin-x86_64-efi/ipxe.efirom  
